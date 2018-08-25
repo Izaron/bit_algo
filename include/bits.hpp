24c868f5a65a3cfdcb30b5ffd4ef653393c84dcc
Evgeny Shulgin <izaronplatz@gmail.com>
2018-08-26 02:20:13 +0300

    Add naive bit_count realization

--- include/bits.hpp
+++ include/bits.hpp
@@ -11,9 +11,11 @@
 #ifndef BIT_ALGORITHM_HPP
 #define BIT_ALGORITHM_HPP
 
+#include <limits>
 #include <boost/detail/workaround.hpp>
+#include <boost/static_assert.hpp>
 
-// Trying to access Microsoft intrinsics
+// Trying to access Microsoft Visual C++ intrinsics
 #if (defined(BOOST_MSVC) && (BOOST_MSVC >= 1600)) || (defined(__clang__) && defined(__c2__)) || (defined(BOOST_INTEL) && defined(_MSC_VER)) && (defined(_M_IX86) || defined(_M_X64))
 #   define BIT_ALGORITHM_MSVC
 #   ifdef _M_X64
@@ -25,6 +27,25 @@
 #endif
 
 namespace bit_algo {
+
+using size_type = std::size_t;
+
+/// \fn bit_count ( IntType value )
+/// \return Number of bits set to 1 in value
+///
+/// \param value    The value to get its bit count
+///     Complexity: O(N).
+template <typename IntType>
+size_type bit_count(IntType value)
+{
+    size_type res = 0;
+    while (value)
+    {
+        res += value & 1;
+        value >>= 1;
+    }
+    return res;
+}
 
 }
 
--- test/main.cpp
+++ test/main.cpp
@@ -4,8 +4,11 @@
   Distributed under the license described in the LICENSE file
 */
 
+#include <iostream>
+
 #include "../include/bits.hpp"
 
 int main() {
+    std::cout << bit_algo::bit_count(228) << std::endl;
     return 0;
 }

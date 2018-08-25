4c7f46d64a021fc9ad346c12c5e2ffc7d726effc
Evgeny Shulgin <izaronplatz@gmail.com>
2018-08-26 01:32:19 +0300

    Add blank files

---
+++ include/bits.hpp
@@ -0,0 +1,14 @@
+/*
+  Copyright (c) Evgeny Shulgin <izaronplatz@gmail.com>, 2018
+
+  Distributed under the license described in the LICENSE file
+*/
+
+/// \file bits.hpp
+/// \brief Provides fast implementations of common bit algorithms.
+/// \author Evgeny Shulgin
+
+#ifndef BIT_ALGORITHM_HPP
+#define BIT_ALGORITHM_HPP
+
+#endif // BIT_ALGORITHM_HPP

bae4dc142e00e35d111ca44ba002e1210c01765c
Evgeny Shulgin <izaronplatz@gmail.com>
2018-08-26 01:53:36 +0300

    Add workaround #defines

--- include/bits.hpp
+++ include/bits.hpp
@@ -11,4 +11,21 @@
 #ifndef BIT_ALGORITHM_HPP
 #define BIT_ALGORITHM_HPP
 
+#include <boost/detail/workaround.hpp>
+
+// Trying to access Microsoft intrinsics
+#if (defined(BOOST_MSVC) && (BOOST_MSVC >= 1600)) || (defined(__clang__) && defined(__c2__)) || (defined(BOOST_INTEL) && defined(_MSC_VER)) && (defined(_M_IX86) || defined(_M_X64))
+#   define BIT_ALGORITHM_MSVC
+#   ifdef _M_X64
+#       define BIT_ALGORITHM_MSVC_64
+#   endif
+// Trying to access GCC intrinsics
+#elif defined(BOOST_GCC) || defined(__clang__) || (defined(BOOST_INTEL) && defined(__GNUC__))
+#   define BIT_ALGORITHM_GCC
+#endif
+
+namespace bit_algo {
+
+}
+
 #endif // BIT_ALGORITHM_HPP
---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+/*
+  Copyright (c) Evgeny Shulgin <izaronplatz@gmail.com>, 2018
+
+  Distributed under the license described in the LICENSE file
+*/
+
+#include "../include/bits.hpp"
+
+int main() {
+    return 0;
+}

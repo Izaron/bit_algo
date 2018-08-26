779b693a2585588eccd9d4dd65df3c88bfa6b4cf
Evgeny Shulgin <izaronplatz@gmail.com>
2018-08-26 19:53:25 +0300

    Add tiny test framework to main.cpp

--- include/bits.hpp
+++ include/bits.hpp
@@ -12,6 +12,8 @@
 #define BIT_ALGORITHM_HPP
 
 #include <boost/detail/workaround.hpp>
+#include <boost/type_traits/is_integral.hpp>
+#include <boost/utility/enable_if.hpp>
 
 // Trying to access Microsoft Visual C++ intrinsics
 #if (defined(BOOST_MSVC) && (BOOST_MSVC >= 1600)) || (defined(__clang__) && defined(__c2__)) || (defined(BOOST_INTEL) && defined(_MSC_VER)) && (defined(_M_IX86) || defined(_M_X64))
@@ -24,6 +26,11 @@
 #   define BIT_ALGORITHM_GCC
 #endif
 
+// Define specifiers for all functions
+// Firstly, it defines a function 'constexpr' if possible
+// Although 'constexpr' implies 'inline', we still define forced inlining
+#define BIT_ALGORITHM_FUNC_SPEC BOOST_CONSTEXPR BOOST_FORCEINLINE
+
 namespace bit_algo {
 
 /// \fn bit_count ( IntType value )
@@ -34,8 +41,9 @@ namespace bit_algo {
 /// \note This function works regardless of the value sign.
 ///     Complexity: O(N).
 template <typename T>
-BOOST_FORCEINLINE typename std::enable_if<std::is_integral<T>::value, int>::type
-bit_count(T value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC typename boost::enable_if<
+    boost::is_integral<T>, int
+>::type bit_count(T value)
 {
     int res = 0;
     while (value)
@@ -50,13 +58,13 @@ bit_count(T value) BOOST_NOEXCEPT
 
 // Using __popcnt variations
 template<>
-BOOST_FORCEINLINE int bit_count(unsigned short value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(unsigned short value)
 {
     return static_cast<int>(__popcnt16(value));
 }
 
 template<>
-BOOST_FORCEINLINE int bit_count(unsigned int value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(unsigned int value)
 {
     return static_cast<int>(__popcnt(value));
 }
@@ -64,14 +72,14 @@ BOOST_FORCEINLINE int bit_count(unsigned int value) BOOST_NOEXCEPT
 #if defined(BIT_ALGORITHM_MSVC_64)
 
 template<>
-BOOST_FORCEINLINE int bit_count(unsigned __int64 value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(unsigned __int64 value)
 {
     return static_cast<int>(__popcnt64(value));
 }
 
 // Casting signed args to unsigned
 template<>
-BOOST_FORCEINLINE int bit_count(__int64 value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(__int64 value)
 {
     return static_cast<unsigned __int64>(bit_count(value));
 }
@@ -80,7 +88,7 @@ BOOST_FORCEINLINE int bit_count(__int64 value) BOOST_NOEXCEPT
 
 // Win32 doesn't have __popcnt64, emulate it with two 32 bit calls
 template<>
-BOOST_FORCEINLINE int bit_count(unsigned long long value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(unsigned long long value)
 {
     return bit_count(static_cast<unsigned int>(value >> 32)) +
         bit_count(static_cast<unsigned int>(value)); // cuts out leading 32 bits
@@ -88,7 +96,7 @@ BOOST_FORCEINLINE int bit_count(unsigned long long value) BOOST_NOEXCEPT
 
 // Casting signed args to unsigned
 template<>
-BOOST_FORCEINLINE int bit_count(long long value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(long long value)
 {
     return static_cast<unsigned long long>(bit_count(value));
 }
@@ -97,13 +105,13 @@ BOOST_FORCEINLINE int bit_count(long long value) BOOST_NOEXCEPT
 
 // Casting signed args to unsigned
 template<>
-BOOST_FORCEINLINE int bit_count(short value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(short value)
 {
     return bit_count(static_cast<unsigned short>(value));
 }
 
 template<>
-BOOST_FORCEINLINE int bit_count(int value) BOOST_NOEXCEPT
+decltype(bit_count<int>) bit_count(int value)
 {
     return bit_count(static_cast<unsigned int>(value));
 }
@@ -112,38 +120,38 @@ BOOST_FORCEINLINE int bit_count(int value) BOOST_NOEXCEPT
 
 // Using __builtin_popcount variations
 template<>
-BOOST_FORCEINLINE int bit_count(unsigned int value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(unsigned int value)
 {
     return __builtin_popcount(value);
 }
 
 template<>
-BOOST_FORCEINLINE int bit_count(unsigned long value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(unsigned long value)
 {
     return __builtin_popcountl(value);
 }
 
 template<>
-BOOST_FORCEINLINE int bit_count(unsigned long long value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(unsigned long long value)
 {
     return __builtin_popcountll(value);
 }
 
 // Casting signed args to unsigned
 template<>
-BOOST_FORCEINLINE int bit_count(int value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(int value)
 {
     return bit_count(static_cast<unsigned int>(value));
 }
 
 template<>
-BOOST_FORCEINLINE int bit_count(long value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(long value)
 {
     return bit_count(static_cast<unsigned long>(value));
 }
 
 template<>
-BOOST_FORCEINLINE int bit_count(long long value) BOOST_NOEXCEPT
+BIT_ALGORITHM_FUNC_SPEC int bit_count(long long value)
 {
     return bit_count(static_cast<unsigned long long>(value));
 }
--- test/main.cpp
+++ test/main.cpp
@@ -5,13 +5,148 @@
 */
 
 #include <iostream>
+#include <limits>
+#include <string>
+#include <vector>
+
+#include <boost/chrono.hpp>
+#include <boost/container/static_vector.hpp>
+#include <boost/random/random_device.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+#include <boost/type_index.hpp>
 
 #include "../include/bits.hpp"
 
-int main() {
-    long kek = 228;
-    int shrek = 1337;
-    std::cout << bit_algo::bit_count(kek) << std::endl;
-    std::cout << bit_algo::bit_count(shrek) << std::endl;
-    return 0;
+const int test_count = 100000;  // 10^5
+//const int test_count = 10000000;  // 10^7
+
+// Naive implementations of common bit algorithms
+namespace naive_impl {
+
+template <typename T>
+int bit_count(T value)
+{
+    int res = 0;
+    while (value)
+    {
+        res += value & 1;
+        value >>= 1;
+    }
+    return res;
+}
+
+}
+
+// Generates test values of the type T,
+// accepts only integral unsigned values
+template<typename T>
+typename boost::enable_if_c<
+    boost::is_integral<T>::value && !std::numeric_limits<T>::is_signed,
+    std::vector<T>
+>::type gen_values()
+{
+    std::vector<T> res;
+    res.reserve(test_count);
+
+    // Using a true random number generator
+    boost::random_device device;
+    boost::random::uniform_int_distribution<T> random(
+        0, std::numeric_limits<T>::max()
+    );
+
+    // Generate values
+    for (int i = 0; i < test_count; ++i) {
+        res.push_back(random(device));
+    }
+    return res;
+}
+
+// Simply runs a function on values
+template<typename T>
+void raw_run(int (*func)(T),
+    const std::vector<T>& vector)
+{
+    // Don't allow the compiler to "optimize" this part
+    // by cutting this out, with "volatile" keyword
+    volatile int res = 0; 
+    for (std::size_t i = 0; i < vector.size(); ++i)
+        res += func(vector[i]);
+}
+
+// Runs functions on test values and measures running time
+template<typename T>
+void run_diff(int (*naive)(T), int (*fast)(T),
+    const std::vector<T>& vector)
+{
+    using namespace boost::chrono;
+
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    raw_run(naive, vector);
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+    duration<double> time_span = duration_cast<
+        duration<double>
+    >(t2 - t1);
+    std::cout << "Naive solution runned " << time_span.count()
+        << " seconds" << std::endl;
+
+    t1 = high_resolution_clock::now();
+    raw_run(fast, vector);
+    t2 = high_resolution_clock::now();
+
+    time_span = duration_cast< duration<double> >(t2 - t1);
+    std::cout << "Fast solution runned  " << time_span.count()
+        << " seconds" << std::endl;
+}
+
+// Asserts equality of both naive (which is correct by default)
+// and fast functions on the same values
+template<typename T>
+void check_equality(int (*naive)(T), int (*fast)(T),
+    const std::vector<T>& vector)
+{
+    for (std::size_t i = 0; i < vector.size(); ++i)
+    {
+        int naive_outcome = naive(vector[i]);
+        int fast_outcome = fast(vector[i]);
+        BOOST_ASSERT(naive_outcome == fast_outcome);
+    }
+}
+
+// Checks both naive and fast functions on correctness
+// and measures their running time
+template<typename T>
+void work_with_functions(const std::string& func_name,
+    int (*naive)(T), int (*fast)(T),
+    const std::vector<T>& vector)
+{
+    std::cout << "WORKING WITH\t\t" << func_name << std::endl;
+    std::cout << "THE TYPE IS\t\t" << boost::typeindex::type_id<T>() << std::endl;
+
+    std::cout << "Check correctness..." << std::endl;
+    check_equality(naive, fast, vector);
+    std::cout << "The fast realization is correct!" << std::endl;
+
+    std::cout << "Measuring time..." << std::endl;
+    run_diff(naive, fast, vector);
+
+    std::cout << std::endl << std::endl;
+}
+
+template<typename T>
+void benchmark()
+{
+    std::vector<T> vector = gen_values<T>();
+
+    work_with_functions("bit_count", naive_impl::bit_count<T>,
+        bit_algo::bit_count<T>, vector);
+}
+
+int main()
+{
+    benchmark<unsigned char>();
+    benchmark<unsigned short>();
+    benchmark<unsigned int>();
+    benchmark<unsigned long>();
+    benchmark<unsigned long long>();
 }
